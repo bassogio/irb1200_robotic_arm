97ade1a4b76eacd87dfc510962f7c536c0017540
bassogio <giordanoschool@gmail.com>
2025-06-22 13:35:02 +0300

    Jacobian

---
+++ Project/4_1_7/Jacobian.m
@@ -0,0 +1,17 @@
+function J = Jacobian(dh, thetas)
+    % Get origins p_i and rotation matrices R_i from FK
+    [~, origins, rots] = forwardKinematics(dh, thetas);
+
+    pe = origins(:,end);          % end-effector origin
+    Jv = zeros(3,6);
+    Jw = zeros(3,6);
+
+    for i = 1:6
+        zi = rots(:,3,i);         % z-axis of joint i (3rd column)
+        pi = origins(:,i);        % origin of joint i
+        Jv(:,i) = cross(zi, pe - pi);
+        Jw(:,i) = zi;
+    end
+
+    J = [Jv; Jw];                 % 6×6 Jacobian
+end
---
+++ Project/4_1_7/TestJacobian.m
@@ -0,0 +1,21 @@
+%% TestJacobian.m
+clc; clear; close all;
+
+% --- Robot DH constants (cm)
+params = struct('d1',399,'a2',350,'a3',42,'d4',351,'d6',82);
+
+% --- Test joint configurations (deg)
+test_angles = [ ...
+     0   0    0   0   0   0;
+    90   0    0   0   0   0;
+    90  90    0   0   0   0;
+    90  90  -90   0   0   0;
+    90  90  -90  90   0   0;
+    90  90  -90  90  90   0;
+    90  90  -90  90  90  90 ];
+
+% --- Pick one of the test poses and compute Jacobian
+q = deg2rad(test_angles(4,:)).';      % 4th pose, column vector
+J = Jacobian(params,q);
+disp('Geometric Jacobian (base frame):');
+disp(J);
